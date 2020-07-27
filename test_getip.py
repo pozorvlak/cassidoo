f8db400f2b41f00fbf3da4183fec1da31d983504
Miles Gould <miles@assyrian.org.uk>
2020-07-27 14:21:48 +0100

    Example answers are in a different order

--- test_getip.py
+++ test_getip.py
@@ -2,10 +2,11 @@ from get_ip import get_ip
 
 
 def test_example():
-    assert list(get_ip('11211')) == [
+    ips = set(get_ip('11211'))
+    assert ips == set([
         '1.1.2.11',
         '1.1.21.1',
         '1.12.1.1',
         '11.2.1.1',
-    ]
+    ])
 
