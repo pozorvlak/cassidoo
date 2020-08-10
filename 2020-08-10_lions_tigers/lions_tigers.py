06c37cd0c6ff415c61c307c6f31ad2a49b538075
Miles Gould <miles@assyrian.org.uk>
2020-08-10 14:19:06 +0100

    Make lions_tigers more cache-friendly

--- 2020-08-10_lions_tigers/lions_tigers.py
+++ 2020-08-10_lions_tigers/lions_tigers.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python
 
 from copy import copy
+from functools import lru_cache
 from sys import argv
 
 
-def num_arrangements(counts, previous_animal=-1):
+@lru_cache()
+def go(counts, previous_count):
     if all(x == 0 for x in counts):
-        return 1
+        return int(previous_count == 0)
     count = 0
     for i, num_animal in enumerate(counts):
-        if num_animal > 0 and i != previous_animal:
-            new_counts = copy(counts)
-            new_counts[i] -= 1
-            count += num_arrangements(new_counts, previous_animal=i)
+        if num_animal > 0:
+            new_counts = tuple(sorted(counts[:i] + counts[i + 1 :] + (previous_count,)))
+            count += go(new_counts, counts[i] - 1)
     return count
 
 
-if __name__ == '__main__':
+def num_arrangements(counts):
+    return sum(
+        go(tuple(sorted(counts[:i] + counts[i + 1 :])), count - 1)
+        for i, count in enumerate(counts)
+        if count > 0
+    )
+
+
+if __name__ == "__main__":
     print(num_arrangements([int(i) for i in argv[1:]]))
