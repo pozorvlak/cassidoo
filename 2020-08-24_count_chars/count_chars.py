ae7cf627d90564e316415e80a73f84a22df74049
Miles Gould <miles@assyrian.org.uk>
2020-08-25 14:00:05 +0100

    Use more_itertools.quantify

--- 2020-08-24_count_chars/count_chars.py
+++ 2020-08-24_count_chars/count_chars.py
@@ -2,13 +2,10 @@
 
 """Given a string s and a character c, return the number of occurrences of c in s"""
 
-def num_chars(s, char):
-    count = 0
-    for c in s:
-        if c == char:
-            count += 1
-    return count
+from more_itertools import quantify
 
+def num_chars(s, char):
+    return quantify(s, lambda c: c == char)
 
 if __name__ == '__main__':
     assert num_chars("oh heavens", "h") == 2
