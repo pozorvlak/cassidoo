bb70e43bfb7554e7ba8f53e8ee3f9f26515672e0
Miles Gould <miles@assyrian.org.uk>
2020-08-31 16:31:16 +0100

    Add Hypothesis "fast == slow" test

--- 2020-08-31_stock_buy_sell/test_stock_buy_sell.py
+++ 2020-08-31_stock_buy_sell/test_stock_buy_sell.py
@@ -1,8 +1,25 @@
 from hypothesis import given
-from hypothesis.strategies import integers
+from hypothesis.strategies import integers, lists
 
 from stock_buy_sell import stock_buy_sell
 
 
+def intervals(n):
+    for i in range(n):
+        for j in range(i+1, n):
+            yield (i, j)
+
+
+def stock_buy_sell_quadratic(prices):
+    n_days = len(prices)
+    buy, sell = max(intervals(n_days), key=lambda xy: prices[xy[1]] - prices[xy[0]])
+    return buy + 1, sell + 1
+
+
 def test_example():
     assert stock_buy_sell([110, 180, 260, 40, 310, 535, 695]) == (4, 7)
+
+
+@given(lists(integers(), min_size=2))
+def test_brute_force(prices):
+    assert stock_buy_sell(prices) == stock_buy_sell_quadratic(prices)
