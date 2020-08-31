9c8b8c8d9154a1104e0bd2cf7547b2e88e410d7d
Miles Gould <miles@assyrian.org.uk>
2020-08-31 16:28:01 +0100

    Tidy up brute-force solution

--- 2020-08-31_stock_buy_sell/stock_buy_sell.py
+++ 2020-08-31_stock_buy_sell/stock_buy_sell.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
-def intervals(xs):
-    for i, x in enumerate(xs):
-        for j, y in enumerate(xs[i+1:]):
-            yield (i, i + 1 + j, y - x)
+def intervals(n):
+    for i in range(n):
+        for j in range(i+1, n):
+            yield (i, j)
 
-def stock_buy_sell(stock_price):
-    buy, sell, profit = max(intervals(stock_price), key=lambda xyz: xyz[2])
+
+def stock_buy_sell(prices):
+    n_days = len(prices)
+    buy, sell = max(intervals(n_days), key=lambda xy: prices[xy[1]] - prices[xy[0]])
     return buy + 1, sell + 1
