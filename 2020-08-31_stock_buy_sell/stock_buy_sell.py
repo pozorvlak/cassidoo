de87b1610702219cb46d5bddbb3e6ff15e7bba12
Miles Gould <miles@assyrian.org.uk>
2020-08-31 17:08:30 +0100

    Eliminate _idx variables
    This only saves one line, because Hypothesis found a bug so I had to
    make the logic more complicated :-(

--- 2020-08-31_stock_buy_sell/stock_buy_sell.py
+++ 2020-08-31_stock_buy_sell/stock_buy_sell.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python
 
 def stock_buy_sell(prices):
-    best_lo = prices[0]
-    best_lo_idx = 0
-    best_hi = prices[1]
-    best_hi_idx = 1
+    best_lo = 0
+    best_hi = 1
     current_lo = best_lo
-    current_lo_idx = best_lo_idx
     current_hi = best_hi
-    current_hi_idx = best_hi_idx
     for i in range(1, len(prices)):
-        if prices[i] > current_hi:
-            current_hi = prices[i]
-            current_hi_idx = i
-            if current_hi - current_lo > best_hi - best_lo:
+        if prices[i] > prices[current_hi]:
+            current_hi = i
+            if (prices[current_hi] - prices[current_lo]
+                    > prices[best_hi] - prices[best_lo]):
                 best_lo = current_lo
-                best_lo_idx = current_lo_idx
                 best_hi = current_hi
-                best_hi_idx = current_hi_idx
-        elif prices[i] < current_lo:
-            current_lo = prices[i]
-            current_lo_idx = i
-            current_hi = prices[i]
-            current_hi_idx = i
-    return best_lo_idx + 1, best_hi_idx + 1
+        elif prices[i] < prices[current_lo] and i < len(prices) - 1:
+            current_lo = i
+            current_hi = i + 1
+            # You'd think this wouldn't be necessary, but consider [1, 0, 0]
+            if (prices[current_hi] - prices[current_lo]
+                    > prices[best_hi] - prices[best_lo]):
+                best_lo = current_lo
+                best_hi = current_hi
+    return best_lo + 1, best_hi + 1
