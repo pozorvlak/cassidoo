28a3002109d8d0083a494d7255086b9de084628c
Miles Gould <miles@assyrian.org.uk>
2020-08-31 16:50:21 +0100

    Ugly O(n) solution to stock_buy_sell

--- 2020-08-31_stock_buy_sell/stock_buy_sell.py
+++ 2020-08-31_stock_buy_sell/stock_buy_sell.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python
 
-def intervals(n):
-    for i in range(n):
-        for j in range(i+1, n):
-            yield (i, j)
-
-
 def stock_buy_sell(prices):
-    n_days = len(prices)
-    buy, sell = max(intervals(n_days), key=lambda xy: prices[xy[1]] - prices[xy[0]])
-    return buy + 1, sell + 1
+    best_lo = prices[0]
+    best_lo_idx = 0
+    best_hi = prices[1]
+    best_hi_idx = 1
+    current_lo = best_lo
+    current_lo_idx = best_lo_idx
+    current_hi = best_hi
+    current_hi_idx = best_hi_idx
+    for i in range(1, len(prices)):
+        if prices[i] > current_hi:
+            current_hi = prices[i]
+            current_hi_idx = i
+            if current_hi - current_lo > best_hi - best_lo:
+                best_lo = current_lo
+                best_lo_idx = current_lo_idx
+                best_hi = current_hi
+                best_hi_idx = current_hi_idx
+        elif prices[i] < current_lo:
+            current_lo = prices[i]
+            current_lo_idx = i
+            current_hi = prices[i]
+            current_hi_idx = i
+    return best_lo_idx + 1, best_hi_idx + 1
