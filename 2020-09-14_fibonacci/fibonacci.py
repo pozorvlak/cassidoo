1b185f679ccda53018c0f06ed3ebe6299811ec1e
Miles Gould <miles@assyrian.org.uk>
2020-09-15 11:29:53 +0100

    Replace nested loop with itertools.combinations

--- 2020-09-14_fibonacci/fibonacci.py
+++ 2020-09-14_fibonacci/fibonacci.py
@@ -10,22 +10,23 @@ $ fibonacciLike([1,3,7,11,12,14,18])
 $ 3 // these sequences: [1,11,12], [3,11,14] or [7,11,18]
 """
 
+from itertools import combinations
+
 
 def fibonacci_like(xs):
     best = 0
     indices = {x: i for i, x in enumerate(xs)}
-    for i in range(len(xs) - 2):
-        for j in range(i + 1, len(xs) - 1):
-            current = 2
-            while True:
-                k = indices.get(xs[i] + xs[j], None)
-                if k is not None:
-                    current += 1
-                    i, j = j, k
-                else:
-                    if current > 2 and current > best:
-                        best = current
-                    break
+    for i, j in combinations(range(len(xs) -1), 2):
+        current = 2
+        while True:
+            k = indices.get(xs[i] + xs[j], None)
+            if k is not None:
+                current += 1
+                i, j = j, k
+            else:
+                if current > 2 and current > best:
+                    best = current
+                break
     return best
 
 
