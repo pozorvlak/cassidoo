eb4251f3d64d85249444fe5b4e2e7bbac5b20662
Miles Gould <miles@assyrian.org.uk>
2020-11-09 19:55:04 +0000

    Add recursive code to calculate counts only

--- 2020-11-09_num_playlists/num_playlists.py
+++ 2020-11-09_num_playlists/num_playlists.py
@@ -13,7 +13,9 @@ import math
 from hypothesis import given
 import hypothesis.strategies as st
 
-
+#====================================================================
+# Solution 1: recursively calculate the actual playlists
+#====================================================================
 def go(available, l, recent, unused):
     if l < 1:
         yield []
@@ -40,6 +42,32 @@ def num_playlists(n, l, k):
     return len(playlists(n, l, k))
 
 
+#====================================================================
+# Solution 2: recursively calculate the number of playlists
+#====================================================================
+def go2(available, l, recent, unused):
+    if l < 1:
+        return 1
+    if l < len(unused):
+        return 0
+    if l == len(unused):
+        return math.factorial(len(unused))
+    b = recent[0]
+    new_available = available | {b} if b is not None else available
+    count = 0
+    for a in available:
+        new_recent = recent[1:] + [a]
+        count += go2(new_available - {a}, l - 1, new_recent, unused - {a})
+    return count
+
+
+def num_playlists2(n, l, k):
+    return go2(set(range(n)), l, [None] * k, set(range(n)))
+
+
+#====================================================================
+# Tests
+#====================================================================
 def test_example():
     assert num_playlists(3, 3, 1) == 6
 
@@ -60,3 +88,21 @@ def test_constraints_hold(n, l, k):
 @given(st.integers(1, 5), st.integers(1, 5), st.integers(1, 5))
 def test_list_too_short(diff, l, k):
     assert num_playlists(l + diff, l, k) == 0
+
+
+def test_example2():
+    assert num_playlists2(3, 3, 1) == 6
+
+
+@given(st.integers(1, 5), st.integers(1, 5))
+def test_n_equals_l(n, k):
+    assert num_playlists2(n, n, k) == math.factorial(n)
+
+
+@given(st.integers(1, 5), st.integers(1, 5), st.integers(1, 5))
+def test_list_too_short(diff, l, k):
+    assert num_playlists2(l + diff, l, k) == 0
+
+@given(st.integers(1, 5), st.integers(1, 5), st.integers(1, 5))
+def test_solutions_agree(n, l, k):
+    assert num_playlists(n, l, k) == num_playlists2(n, l, k)
