90831e9125e39d85b314698187dce2e1fb54f76b
Miles Gould <miles@assyrian.org.uk>
2021-03-29 11:02:47 +0100

    Add solution to valid_number problem

---
+++ 2021-03-29_valid_number/valid_number.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import re
+
+import pytest
+
+
+num_re = re.compile(r'[+\-]?(\d+\.?\d*|\.\d+)')
+
+
+def valid_num(s):
+    return num_re.fullmatch(s) is not None
+
+
+valid = ["7", "0011", "+3.14", "4.", "-.9", "-123.456", "-0.1"]
+invalid = ["abc", "1a", "e8", "–6", "-+3", "95x54e53."]
+
+
+@pytest.mark.parametrize("s", valid)
+def test_valid(s):
+    assert valid_num(s)
+
+
+@pytest.mark.parametrize("s", invalid)
+def test_invalid(s):
+    assert not valid_num(s)
