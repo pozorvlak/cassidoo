8c6aa4afaeb586d2fc79c08c97de6fb03d823169
Miles Gould <miles@assyrian.org.uk>
2021-05-24 12:18:07 +0100

    Add Connect 4 game

---
+++ 2021-05-24_connect_four/connect_four.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+"""
+Make a 2-player Connect Four game, where the board is 7 units wide, and 6 units
+high. Your program should take in user input when a player puts in a piece, and
+have functions boardState() (which returns a 2D array of the current state of
+the board) and hasWon() which returns which player has won the game (if any).
+"""
+
+BOARD = [[" " for i in range(7)] for j in range(6)]
+PLAYERS = ["Red", "Yellow"]
+TOKENS = "RY"
+winner = None
+
+
+def boardState():
+    for row in BOARD:
+        print("".join(row))
+    print("0123456")
+
+
+def hasWon():
+    return winner
+
+
+def highest_filled_row(column):
+    for i in range(6):
+        if BOARD[i][column] != " ":
+            return i
+    return 6
+
+
+def drop_token(column, player):
+    global winner
+    token = TOKENS[player]
+    row = highest_filled_row(column) - 1
+    BOARD[row][column] = token
+    if row <= 3 and all((BOARD[row + i][column] == token for i in range(4))):
+        winner = player
+        return
+    for i in range(4):
+        c = column - i
+        if c < 0 or c > 3:
+            continue
+        if all((BOARD[row][c + j] == token for j in range(4))):
+            winner = player
+            return
+    for i in range(4):
+        sr = row - i
+        if sr < 0 or sr > 2:
+            continue
+        scl = column - i
+        scr = column + i
+        if 0 <= scl <= 3 and all((BOARD[sr + j][scl + j] == token for j in range(4))):
+            winner = player
+            return
+        if 3 <= scr <= 6 and all((BOARD[sr + j][scr - j] == token for j in range(4))):
+            winner = player
+            return
+
+
+player = 1
+while hasWon() is None:
+    boardState()
+    player = 1 - player
+    while True:
+        column = int(input(f"{PLAYERS[player]} to move:"))
+        if not (0 <= column <= 6):
+            print("Please enter a number between 0 and 6")
+        elif highest_filled_row(column) <= 0:
+            print(f"Column {column} is already full")
+        else:
+            break
+    drop_token(column, player)
+
+boardState()
+print(f"{PLAYERS[hasWon()]} has won!")
