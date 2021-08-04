4044e42bc004fa5d115bef0dd3b17531f4c3d830
Miles Gould <miles@assyrian.org.uk>
2021-08-04 10:26:21 +0100

    Add brute-force solution to missing 7s problem

--- 2021-08-03_missing_sevens/missing_sevens.py
+++ 2021-08-03_missing_sevens/missing_sevens.py
@@ -15,7 +15,43 @@ $ missingSevens([[3,9,6],[8,5,5],[8,4,0]])
 $ 2
 """
 
+from copy import deepcopy
 
+
+def bitstrings(n, ones, prefix=[]):
+    if n == 0:
+        yield prefix
+        return
+    if ones > 0:
+        if ones == n:
+            yield prefix + ([1] * ones)
+            return
+        yield from bitstrings(n - 1, ones - 1, prefix + [1])
+    yield from bitstrings(n - 1, ones, prefix + [0])
+
+
+def reshape(xss):
+    return [xss[3*i:3*i+3] for i in range(3)]
+
+
+def is_latin(matrix):
+    row_sums = {sum(row) for row in matrix}
+    col_sums = {sum(row[i] for row in matrix) for i in range(3)}
+    sums = row_sums | col_sums
+    return len(sums) == 1
+
+
+def missing_sevens(matrix):
+    for num_sevens in range(10):
+        for bs in bitstrings(9, num_sevens):
+            mask = reshape(bs)
+            altered = deepcopy(matrix)
+            for i in range(3):
+                for j in range(3):
+                    if mask[i][j]:
+                        altered[i][j] = 7
+            if is_latin(altered):
+                return num_sevens
 
 
 def test_example1():
