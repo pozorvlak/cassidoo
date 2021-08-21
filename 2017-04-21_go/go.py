70d65c5787a9aa6f0de2e8c3ecbbc3b11fb3a9a8
Miles Gould <miles@assyrian.org.uk>
2021-08-21 21:17:40 +0100

    Add placeholder for Go problem

---
+++ 2017-04-21_go/go.py
@@ -0,0 +1,8 @@
+"""
+Write a data structure for the board game Go.  Write the functions for placing
+a piece and capturing a territory.
+"""
+
+class Goban:
+    def __init__(self, size):
+        # XXX come back to this one
