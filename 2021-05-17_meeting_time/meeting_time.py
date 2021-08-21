63c4800ea787b88cd49800d014f3bda3323e99d6
Miles Gould <miles@assyrian.org.uk>
2021-08-21 21:19:17 +0100

    Add placeholder for meeting time problem

---
+++ 2021-05-17_meeting_time/meeting_time.py
@@ -0,0 +1,8 @@
+"""
+Given a list of people with their names and the time slots when they are
+available in a given week, and a meeting length, return the first meeting time
+available (if possible). Format the input however you’d like to receive it!
+"""
+
+# XXX solve me!
+
