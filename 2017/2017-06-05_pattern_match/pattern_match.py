d178f24fc57567d85b31a02d48db6be6a0841acc
Miles Gould <miles@assyrian.org.uk>
2021-09-06 11:30:50 +0100

    WIP hand-rolled solution to pattern match problem

--- 2017/2017-06-05_pattern_match/pattern_match.py
+++ 2017/2017-06-05_pattern_match/pattern_match.py
@@ -11,9 +11,24 @@ false
 
 from collections import Counter
 import re
+from string import ascii_lowercase
+
+from hypothesis import given, strategies as st
+
+
+def replace_all(pattern, assignments):
+    for key, value in assignments.items():
+        pattern = pattern.replace(key, value)
+    return pattern
 
 
 def pattern_match(pattern, s):
+    uses = Counter(pattern)
+    # XXX finish me!
+    return False
+
+
+def oracle(pattern, s):
     regex = pattern
     for i, char in enumerate(Counter(pattern)):
         regex = regex.replace(char, "(.*?)", 1)
@@ -29,3 +44,17 @@ def test_example1():
 
 def test_example2():
     assert not pattern_match('AA', 'fishyfish')
+
+
+word = st.text(alphabet=ascii_lowercase)
+
+
+@given(st.text(alphabet="ABCDE", max_size=10), word)
+def test_oracle(pattern, s):
+    assert oracle(pattern, s) == pattern_match(pattern, s)
+
+
+@given(st.text(alphabet="ABC", max_size=10), word, word, word)
+def test_known_good(pattern, a, b, c):
+    s = pattern.replace("A", a).replace("B", b).replace("C", c)
+    assert pattern_match(pattern, s)
