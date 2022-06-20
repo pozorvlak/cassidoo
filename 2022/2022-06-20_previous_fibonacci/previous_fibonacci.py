4da3aab54f3fd5d1aaa6dcb885b134d1eba4ed19
Miles Gould <miles@assyrian.org.uk>
2022-06-20 10:51:28 +0100

    Add solution to "previous Fibonacci number" problem

---
+++ 2022/2022-06-20_previous_fibonacci/previous_fibonacci.py
@@ -0,0 +1,47 @@
+"""
+Given a Fibonacci number, give the previous Fibonacci number. If the number
+given is not a Fibonacci number, return -1.
+
+Uses formulae from https://r-knott.surrey.ac.uk/Fibonacci/fibFormula.html .
+"""
+
+from math import sqrt
+
+
+def is_square(n):
+    "Is n a square number?"
+    s = sqrt(n)
+    return int(s) == s
+
+
+def is_fib(n):
+    "Is n a Fibonacci number?"
+    return is_square(5 * n * n + 4) or is_square(5 * n * n - 4)
+
+
+def previous_fibonacci(n):
+    "If n is a Fibonacci number return the previous one, else return -1"
+    phi = (sqrt(5) + 1) / 2
+    if is_fib(n):
+        return round(n / phi)
+    return -1
+
+
+def test_example1():
+    assert previous_fibonacci(2) == 1
+
+
+def test_example2():
+    assert previous_fibonacci(13) == 8
+
+
+def test_example3():
+    assert previous_fibonacci(12) == -1
+
+
+def test_oracle_fib():
+    x, y = 1, 1
+    for i in range(20):
+        assert previous_fibonacci(y) == x
+        x, y = y, x + y
+
