cb719b2c380d03a7b0cd9597cf494a9c3e809ab1
Miles Gould <miles@assyrian.org.uk>
2024-04-15 14:20:53 +0100

    Add linear solution to unique_substr

--- 2024/04-15_unique_substr/unique_substr.py
+++ 2024/04-15_unique_substr/unique_substr.py
@@ -1,5 +1,6 @@
 """
-Given a string str, write a function to determine the longest substring containing only two unique characters.
+Given a string str, write a function to determine the longest substring
+containing only two unique characters.
 
 Example:
 
@@ -17,17 +18,33 @@ from hypothesis import given, strategies as st
 
 
 def unique_substr(word):
+    if len(word) <= 2:
+        return len(word)
+    current, other = word[0], None
+    current_start, pair_start = 0, 0
+    best_length = 0
+    for i, c in enumerate(word):
+        if c != current:
+            if other is not None and c != other:
+                # Finish the current two-character run
+                best_length = max(best_length, i - pair_start)
+                pair_start = current_start
+            current, other = c, current
+            current_start = i
+    best_length = max(best_length, len(word) - pair_start)
+    return best_length
+
+
+def oracle(word):
+    if len(word) <= 2:
+        return len(word)
     best_known = 2
     for i in range(len(word) - 1):
         # no point considering substrings shorter than best_known
         for j in range(i + best_known + 1, len(word) + 1):
-            if len(set(word[i:j])) == 2:
+            if len(set(word[i:j])) <= 2:
                 best_known = j - i
     return best_known
-
-
-def oracle(word):
-    return unique_substr(word)
 
 
 def test_example1():
@@ -40,6 +57,10 @@ def test_example2():
 
 def test_example3():
     assert unique_substr('abcabcabc') == 2 # "ab", "bc", or "ca"
+
+
+def test_example4():
+    assert unique_substr('aab') == 3
 
 
 @given(word=st.text('abcdefghijklmnopqrstuvwxyz'))
