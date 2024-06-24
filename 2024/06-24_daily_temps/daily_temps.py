b2d60aa20501c39439fad47b28df5af32b2ce27d
Miles Gould <miles@assyrian.org.uk>
2024-06-24 11:15:09 +0100

    Add O(n log n) solution to daily_temperatures

--- 2024/06-24_daily_temps/daily_temps.py
+++ 2024/06-24_daily_temps/daily_temps.py
@@ -16,7 +16,30 @@ Example:
 > [1, 1, 4, 2, 1, 1, 0, 0]
 """
 
+from bisect import bisect_right
+
+from hypothesis import given, strategies as st
+
+
 def daily_temperatures(temps):
+    seen = []
+    indices = []
+    output = [None for t in temps]
+    for i in range(len(temps) - 1, -1, -1):
+        t = temps[i]
+        # Find the next strictly larger temperature among those we've seen
+        j = bisect_right(seen, t)
+        if j >= len(seen):
+            output[i] = 0
+        else:
+            output[i] = indices[j] - i
+        # Drop all seen values <= t, they're now dominated by t
+        seen = [t] + seen[j:]
+        indices = [i] + indices[j:]
+    return output
+
+
+def oracle(temps):
     output = [0 for t in temps]
     for i, t1 in enumerate(temps):
         for j, t2 in enumerate(temps[i:]):
@@ -39,3 +62,8 @@ def test_example2():
 def test_example3():
     assert daily_temperatures([73, 74, 75, 71, 69, 72, 76, 73]) \
             == [1, 1, 4, 2, 1, 1, 0, 0]
+
+
+@given(st.lists(st.integers(), max_size=10))
+def test_oracle(temps):
+    assert daily_temperatures(temps) == oracle(temps)
